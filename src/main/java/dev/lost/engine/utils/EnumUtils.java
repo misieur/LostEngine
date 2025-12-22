4e9e01c32d2b09728ff1051d9cf9c6c917b18635
misieur <misieur.off@gmail.com>
2025-12-22 18:07:24 +0100

    Much better

--- src/main/java/dev/lost/engine/utils/EnumUtils.java
+++ src/main/java/dev/lost/engine/utils/EnumUtils.java
@@ -1,13 +1,12 @@
 package dev.lost.engine.utils;
 
-import java.util.Locale;
 import java.util.Optional;
 
 public class EnumUtils {
     public static <T extends Enum<T>> Optional<T> match(String key, Class<T> enumClass) {
         try {
-            return Optional.of(Enum.valueOf(enumClass, key.toUpperCase(Locale.ROOT)));
-        } catch (NullPointerException | IllegalArgumentException e) {
+            return Optional.of(Enum.valueOf(enumClass, key.toUpperCase()));
+        } catch (Throwable e) {
             return Optional.empty();
         }
     }
